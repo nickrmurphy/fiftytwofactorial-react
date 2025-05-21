3d555506a5828f593729930940d4002516cda9fb
nickrmurphy <hi@nickmurphy.io>
2025-05-21 14:29:32 -0500

    Switch to cloudflare

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,7 @@
 // app.config.ts
 import { defineConfig } from "@tanstack/react-start/config";
 import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "unenv";
 
 export default defineConfig({
 	tsr: {
@@ -10,6 +11,7 @@ export default defineConfig({
 		plugins: [tailwindcss()],
 	},
 	server: {
-		preset: "netlify",
+		preset: "cloudflare-pages",
+		unenv: cloudflare,
 	},
 });
